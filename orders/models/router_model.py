2a7b4673efaff45a06ca8eefcc3b11036a6cafd4
Evangelos Kalimeris <vageliskalimeris@gmail.com>
2024-06-22 15:12:31 +0200

    Commit models.

---
+++ orders/models/router_model.py
@@ -0,0 +1,50 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class OrderInput(BaseModel):
+    stocks: str = Field(
+        description="Currency pair symbol (e.g. 'EURUSD'), or any other stuff",
+        json_schema_extra={'example': 'EURUSD'}
+    )
+    quantity: float = Field(
+        description='Quantity of the currency pair to be traded',
+        json_schema_extra={'example': 50}
+    )
+
+
+class Status(Enum):
+    pending = 'pending'
+    executed = 'executed'
+    canceled = 'canceled'
+
+
+class OrderOutput(BaseModel):
+    id: str = Field(
+        description='Unique identifier for the order',
+        json_schema_extra={'example': '3a051d9d-5932-4b40-8c61-e2ebb673bd31'},
+    )
+    stocks: str = Field(
+        description="Currency pair symbol (e.g. 'EURUSD')",
+        json_schema_extra={'example': 'EURUSD'}
+    )
+    quantity: float = Field(
+        description='Quantity of the currency pair to be traded',
+        json_schema_extra={'example': 50}
+    )
+    status: Status = Field(
+        description='Status of the order',
+        json_schema_extra={'example': Status.executed}
+    )
+
+
+class Error(BaseModel):
+    code: int = Field(
+        description='Error code',
+        json_schema_extra={'example': 404}
+    )
+    message: str = Field(
+        description='Error message',
+        json_schema_extra={'example': 'Order not found'}
+    )
