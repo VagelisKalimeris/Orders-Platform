a41e939c562611e6eeaf60a988e31a4652f9b306
Evangelos Kalimeris <vageliskalimeris@gmail.com>
2024-06-22 14:25:36 +0200

    Initialize routes. Implement models. Add docker infrastructure.

---
+++ orders/main.py
@@ -0,0 +1,39 @@
+################################################################################
+#         For API trial and documentation visit http://localhost/docs          #
+################################################################################
+
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
+from orders.routers import order_router
+
+
+def customize_openapi_schema():
+    """
+    Updates OpenAPI documentation details for SWAGGER UI.
+    """
+    openapi_schema = get_openapi(
+        title='Forex Trading Platform API',
+        version='1.0.0',
+        description='A RESTful API to simulate a Forex trading platform with WebSocket support for real-time order '
+                    'updates.',
+        servers=[
+            {
+                'url': 'http://0.0.0.0:80'
+            }
+        ],
+        routes=app.routes
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+# Start API
+app = FastAPI()
+
+# Enable routes
+app.include_router(order_router.router)
+
+
+# Customize SWAGGER
+app.openapi = customize_openapi_schema
---
+++ orders/routers/order_router.py
@@ -0,0 +1,47 @@
+from typing import List
+from fastapi import APIRouter, HTTPException, WebSocket
+
+from orders.models.router_model import OrderInput, OrderOutput, Status
+
+router = APIRouter()
+
+
+
+order_example = OrderOutput(id='33', stocks='fff', quantity=50, status=Status.pending)
+
+
+@router.get('/orders', status_code=200)
+async def retrieve_all_orders() -> List[OrderOutput]:
+    """
+    """
+
+    return [order_example]
+
+
+@router.post('/orders', status_code=201)
+async def place_a_new_order(order_info: OrderInput) -> OrderOutput:
+    """
+    """
+
+    return order_example
+
+@router.get('/orders/{order_id}', status_code=200)
+async def retrieve_a_specific_order(order_id: str) -> OrderOutput:
+    """
+    """
+
+    return order_example
+
+
+@router.delete('/orders/{order_id}', status_code=204)
+async def cancel_an_order(order_id: str) -> None:
+    """
+    """
+    pass
+
+
+@router.websocket('/ws')
+async def web_socket_connection_for_real_time_order_information() -> None:
+    """
+    """
+    pass
