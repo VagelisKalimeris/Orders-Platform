91cacc711e3760ec38727aa3a9d613dbaeb399a6
Evangelos Kalimeris <vageliskalimeris@gmail.com>
2024-06-28 17:31:33 +0200

    Update websocket test main.

--- test/websocket_tests/test_websocket.py
+++ test/websocket_tests/test_websocket.py
@@ -1,12 +1,13 @@
-from httpx import AsyncClient
 
+from httpx_ws import connect_ws
 
 """
 A main purely for debugging.
 """
+
 if __name__ == '__main__':
-    client = AsyncClient()
-    ws = client.websocket('0.0.0.0:80/ws')
-    ws.send_text('put uuid here')
-    received_status = ws.receive_text()
-    print(f'Status: {received_status}')
+
+    with connect_ws("http://0.0.0.0:80/ws") as ws:
+        while True:
+            message = ws.receive_text()
+            print(message)
